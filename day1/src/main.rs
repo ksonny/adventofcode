30be2bb8643ff94b9cb30e45e069143dd13b390f
Sonny Karlsson <ksonny@lotrax.org>
2020-12-20 13:26:15 +0100

    Add rustfmt

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,77 +1,81 @@
 use io::Lines;
-use nom::character::complete::digit1;
-use nom::combinator::map_res;
-use nom::combinator::recognize;
-use nom::IResult;
+use nom::{
+	character::complete::digit1,
+	combinator::{map_res, recognize},
+	IResult,
+};
 use std::{
-    env,
-    io::{self, BufRead},
+	env,
+	io::{self, BufRead},
 };
 
 fn number(i: &str) -> IResult<&str, u64> {
-    map_res(recognize(digit1), str::parse)(i)
+	map_res(recognize(digit1), str::parse)(i)
 }
 
 fn read_input<R: BufRead>(lines: Lines<R>) -> Vec<u64> {
-    lines
-        .filter_map(|r| r.map_or(None, |s| Some(s)))
-        .filter_map(|s| number(&s).map_or(None, |(_, n)| Some(n)))
-        .collect::<Vec<_>>()
+	lines
+		.filter_map(|r| r.map_or(None, |s| Some(s)))
+		.filter_map(|s| number(&s).map_or(None, |(_, n)| Some(n)))
+		.collect::<Vec<_>>()
 }
 
 fn find_day1_part1_num(input: &mut Vec<u64>, sum: u64) -> Option<u64> {
-    input.iter().enumerate().find_map(|(i, m)| {
-        input
-            .iter()
-            .skip(i)
-            .find_map(|&n| if (m + n) == sum { Some(m * n) } else { None })
-    })
+	input.iter().enumerate().find_map(|(i, m)| {
+		input.iter().skip(i).find_map(|&n| {
+			if (m + n) == sum {
+				Some(m * n)
+			} else {
+				None
+			}
+		})
+	})
 }
 
 fn find_day1_part2_num(input: &mut Vec<u64>, sum: u64) -> Option<u64> {
-    input.iter().enumerate().find_map(|(i, m)| {
-        input.iter().enumerate().skip(i).find_map(|(j, n)| {
-            input.iter().skip(j).find_map(|&o| {
-                if (m + n + o) == sum {
-                    Some(m * n * o)
-                } else {
-                    None
-                }
-            })
-        })
-    })
+	input.iter().enumerate().find_map(|(i, m)| {
+		input.iter().enumerate().skip(i).find_map(|(j, n)| {
+			input.iter().skip(j).find_map(|&o| {
+				if (m + n + o) == sum {
+					Some(m * n * o)
+				} else {
+					None
+				}
+			})
+		})
+	})
 }
 
 fn main() {
-    let args = env::args().collect::<Vec<_>>();
-    let sum = args
-        .get(1)
-        .and_then(|s| number(s).map(|(_, u)| Some(u)).unwrap_or(None))
-        .unwrap_or(2020);
+	let args = env::args().collect::<Vec<_>>();
+	let sum = args
+		.get(1)
+		.and_then(|s| number(s).map(|(_, u)| Some(u)).unwrap_or(None))
+		.unwrap_or(2020);
 
-    let input = read_input(io::stdin().lock().lines());
-    let part1_answer = find_day1_part1_num(&mut input.clone(), sum);
-    let part2_answer = find_day1_part2_num(&mut input.clone(), sum);
+	let input = read_input(io::stdin().lock().lines());
+	let part1_answer = find_day1_part1_num(&mut input.clone(), sum);
+	let part2_answer = find_day1_part2_num(&mut input.clone(), sum);
 
-    if let Some(x) = part1_answer {
-        println!("part1 answer: {}", x);
-    }
-    if let Some(x) = part2_answer {
-        println!("part2 answer: {}", x);
-    }
+	if let Some(x) = part1_answer {
+		println!("part1 answer: {}", x);
+	}
+	if let Some(x) = part2_answer {
+		println!("part2 answer: {}", x);
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use std::io::Write;
+	use super::*;
+	use std::io::Write;
 
-    #[test]
-    fn test_read_input() {
-        let mut buffer = Vec::new();
-        buffer.write_all(b"0\n1\n2\n3\n4\n").unwrap();
+	#[test]
+	fn test_read_input() {
+		let mut buffer = Vec::new();
+		buffer.write_all(b"0\n1\n2\n3\n4\n").unwrap();
 
-        let input = read_input(buffer.lines());
-        assert_eq!(input, vec![0, 1, 2, 3, 4]);
-    }
+		let input = read_input(buffer.lines());
+		assert_eq!(input, vec![0, 1, 2, 3, 4]);
+	}
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,184 +1,188 @@
 use io::Lines;
-use nom::combinator::map;
-use nom::combinator::map_res;
-use nom::combinator::recognize;
-use nom::IResult;
 use nom::{
-    bytes::complete::tag,
-    character::complete::{alphanumeric1, anychar, digit1, multispace0},
-    sequence::{preceded, terminated, tuple},
+	bytes::complete::tag,
+	character::complete::{alphanumeric1, anychar, digit1, multispace0},
+	combinator::{map, map_res, recognize},
+	sequence::{preceded, terminated, tuple},
+	IResult,
 };
 use std::io::{self, BufRead};
 
 #[derive(Debug, PartialEq)]
 struct Entry {
-    upper: u64,
-    lower: u64,
-    char: char,
-    password: String,
+	upper: u64,
+	lower: u64,
+	char: char,
+	password: String,
 }
 
 fn number1(i: &str) -> IResult<&str, u64> {
-    map_res(recognize(digit1), str::parse)(i)
+	map_res(recognize(digit1), str::parse)(i)
 }
 
 fn password1(i: &str) -> IResult<&str, &str> {
-    alphanumeric1(i)
+	alphanumeric1(i)
 }
 
 fn entry(i: &str) -> IResult<&str, Entry> {
-    map(
-        tuple((
-            terminated(number1, tag("-")),
-            terminated(number1, multispace0),
-            terminated(anychar, tag(":")),
-            preceded(multispace0, password1),
-        )),
-        |(l, u, c, p)| Entry {
-            lower: l,
-            upper: u,
-            char: c,
-            password: p.into(),
-        },
-    )(i)
+	map(
+		tuple((
+			terminated(number1, tag("-")),
+			terminated(number1, multispace0),
+			terminated(anychar, tag(":")),
+			preceded(multispace0, password1),
+		)),
+		|(l, u, c, p)| Entry {
+			lower: l,
+			upper: u,
+			char: c,
+			password: p.into(),
+		},
+	)(i)
 }
 
 fn read_input<R: BufRead>(lines: Lines<R>) -> impl Iterator<Item = Entry> {
-    lines
-        .filter_map(|r| r.map_or(None, |s| Some(s)))
-        .filter_map(|s| entry(&s).map_or(None, |(_, n)| Some(n)))
+	lines
+		.filter_map(|r| r.map_or(None, |s| Some(s)))
+		.filter_map(|s| entry(&s).map_or(None, |(_, n)| Some(n)))
 }
 
 fn valid_day2_part1(entry: &Entry) -> bool {
-    let char_count = entry
-        .password
-        .chars()
-        .fold(0, |acc, c| if c == entry.char { acc + 1 } else { acc });
-    char_count >= entry.lower && char_count <= entry.upper
+	let char_count = entry.password.chars().fold(0, |acc, c| {
+		if c == entry.char {
+			acc + 1
+		} else {
+			acc
+		}
+	});
+	char_count >= entry.lower && char_count <= entry.upper
 }
 
 fn valid_day2_part2(entry: &Entry) -> bool {
-    entry
-        .password
-        .char_indices()
-        .filter(|&(i, _)| entry.lower as usize == i + 1 || entry.upper as usize == i + 1)
-        .filter(|&(_, c)| c == entry.char)
-        .count()
-        == 1
+	entry
+		.password
+		.char_indices()
+		.filter(|&(i, _)| {
+			entry.lower as usize == i + 1 || entry.upper as usize == i + 1
+		})
+		.filter(|&(_, c)| c == entry.char)
+		.count() == 1
 }
 
 fn main() {
-    let stdin = io::stdin();
-    let entries = read_input(stdin.lock().lines()).collect::<Vec<_>>();
+	let stdin = io::stdin();
+	let entries = read_input(stdin.lock().lines()).collect::<Vec<_>>();
 
-    let valid_entries_count = entries.iter().filter(|e| valid_day2_part1(&e)).count();
-    println!("part1 answer: {}", valid_entries_count);
-    let valid_entries_count = entries.iter().filter(|e| valid_day2_part2(&e)).count();
-    println!("part2 answer: {}", valid_entries_count);
+	let valid_entries_count =
+		entries.iter().filter(|e| valid_day2_part1(&e)).count();
+	println!("part1 answer: {}", valid_entries_count);
+	let valid_entries_count =
+		entries.iter().filter(|e| valid_day2_part2(&e)).count();
+	println!("part2 answer: {}", valid_entries_count);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn entry_parse() {
-        let input = "1-3 a: aaa";
-        let e = Entry {
-            lower: 1,
-            upper: 3,
-            char: 'a',
-            password: "aaa".into(),
-        };
-        let (_, r) = entry(input).unwrap();
-        assert_eq!(r, e);
-    }
+	#[test]
+	fn entry_parse() {
+		let input = "1-3 a: aaa";
+		let e = Entry {
+			lower: 1,
+			upper: 3,
+			char: 'a',
+			password: "aaa".into(),
+		};
+		let (_, r) = entry(input).unwrap();
+		assert_eq!(r, e);
+	}
 
-    #[test]
-    fn test_valid_day2_trivial() {
-        let entry = Entry {
-            lower: 1,
-            upper: 1,
-            char: 'a',
-            password: "a".into(),
-        };
-        assert!(valid_day2_part1(&entry));
-    }
+	#[test]
+	fn test_valid_day2_trivial() {
+		let entry = Entry {
+			lower: 1,
+			upper: 1,
+			char: 'a',
+			password: "a".into(),
+		};
+		assert!(valid_day2_part1(&entry));
+	}
 
-    #[test]
-    fn test_valid_day2_between() {
-        let entry = Entry {
-            lower: 1,
-            upper: 3,
-            char: 'a',
-            password: "aa".into(),
-        };
-        assert!(valid_day2_part1(&entry));
-    }
+	#[test]
+	fn test_valid_day2_between() {
+		let entry = Entry {
+			lower: 1,
+			upper: 3,
+			char: 'a',
+			password: "aa".into(),
+		};
+		assert!(valid_day2_part1(&entry));
+	}
 
-    #[test]
-    fn test_valid_day2_no_matching() {
-        let entry = Entry {
-            lower: 1,
-            upper: 1,
-            char: 'a',
-            password: "b".into(),
-        };
-        assert!(!valid_day2_part1(&entry));
-    }
+	#[test]
+	fn test_valid_day2_no_matching() {
+		let entry = Entry {
+			lower: 1,
+			upper: 1,
+			char: 'a',
+			password: "b".into(),
+		};
+		assert!(!valid_day2_part1(&entry));
+	}
 
-    #[test]
-    fn test_valid_day2_too_many() {
-        let entry = Entry {
-            lower: 1,
-            upper: 1,
-            char: 'a',
-            password: "aa".into(),
-        };
-        assert!(!valid_day2_part1(&entry));
-    }
+	#[test]
+	fn test_valid_day2_too_many() {
+		let entry = Entry {
+			lower: 1,
+			upper: 1,
+			char: 'a',
+			password: "aa".into(),
+		};
+		assert!(!valid_day2_part1(&entry));
+	}
 
-    #[test]
-    fn test_valid_day2_too_few() {
-        let entry = Entry {
-            lower: 3,
-            upper: 3,
-            char: 'a',
-            password: "aa".into(),
-        };
-        assert!(!valid_day2_part1(&entry));
-    }
+	#[test]
+	fn test_valid_day2_too_few() {
+		let entry = Entry {
+			lower: 3,
+			upper: 3,
+			char: 'a',
+			password: "aa".into(),
+		};
+		assert!(!valid_day2_part1(&entry));
+	}
 
-    #[test]
-    fn test_valid_day2_part2_a() {
-        let entry = Entry {
-            lower: 1,
-            upper: 3,
-            char: 'a',
-            password: "abcde".into(),
-        };
-        assert!(valid_day2_part2(&entry));
-    }
+	#[test]
+	fn test_valid_day2_part2_a() {
+		let entry = Entry {
+			lower: 1,
+			upper: 3,
+			char: 'a',
+			password: "abcde".into(),
+		};
+		assert!(valid_day2_part2(&entry));
+	}
 
-    #[test]
-    fn test_valid_day2_part2_b() {
-        let entry = Entry {
-            lower: 1,
-            upper: 3,
-            char: 'b',
-            password: "cdefg".into(),
-        };
-        assert!(!valid_day2_part2(&entry));
-    }
+	#[test]
+	fn test_valid_day2_part2_b() {
+		let entry = Entry {
+			lower: 1,
+			upper: 3,
+			char: 'b',
+			password: "cdefg".into(),
+		};
+		assert!(!valid_day2_part2(&entry));
+	}
 
-    #[test]
-    fn test_valid_day2_part2_c() {
-        let entry = Entry {
-            lower: 2,
-            upper: 9,
-            char: 'c',
-            password: "ccccccccc".into(),
-        };
-        assert!(!valid_day2_part2(&entry));
-    }
+	#[test]
+	fn test_valid_day2_part2_c() {
+		let entry = Entry {
+			lower: 2,
+			upper: 9,
+			char: 'c',
+			password: "ccccccccc".into(),
+		};
+		assert!(!valid_day2_part2(&entry));
+	}
 }
